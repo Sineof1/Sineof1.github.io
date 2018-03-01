11b38d32b7ffd9d74fae1d1cea4ce7e7fdd7883b
Sineof1 <Sineof1@users.noreply.github.com>
2018-03-01 12:30:03 -0500

    Update tri_ineq_script.js

--- tri_ineq_script.js
+++ tri_ineq_script.js
@@ -5,8 +5,7 @@ var svgTriIneq = d3.select('#svgAnimInsert')
                    .append('svg')
                    .attr('width', 300)
                    .attr('height', 200)
-                   .style('transform-origin', 'center')
-                   .attr('transform', 'scale(2, 2)');
+                   .attr('transform', 'translate(50%, 50%) scale(2, 2)');
 
 svgTriIneq.append('line')
           .attr('x1', 40)
