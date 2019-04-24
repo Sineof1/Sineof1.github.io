c3cb994ef724168850a45948f07e3cd9ab861d67
Sineof1 <Sineof1@users.noreply.github.com>
2019-04-23 21:53:25 -0400

    Update tutorbot.js

--- tutorbot/tutorbot.js
+++ tutorbot/tutorbot.js
@@ -582,15 +582,6 @@ function hintGroup3() {
 }
 
 interact('.resize-drag')
-  .draggable({
-    onmove: window.dragMoveListener,
-    modifiers: [
-      interact.modifiers.restrict({
-        restriction: 'parent',
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-      })
-    ]
-  })
   .resizable({
     // resize from all edges and corners
     edges: { left: false, right: false, bottom: true, top: true },
@@ -614,19 +605,4 @@ interact('.resize-drag')
     var target = event.target,
         x = (parseFloat(target.getAttribute('data-x')) || 0),
         y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-    // update the element's style
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
-
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
-
-    target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
-
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
   });
