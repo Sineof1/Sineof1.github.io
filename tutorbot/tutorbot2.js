3a33551023075dc65e351a396ee00a9f94db3228
Sineof1 <Sineof1@users.noreply.github.com>
2019-05-02 15:42:03 -0400

    Update tutorbot2.js

--- tutorbot/tutorbot2.js
+++ tutorbot/tutorbot2.js
@@ -15,7 +15,7 @@ botresponse.volume = 0.2;
 var ding = document.getElementById('ding');
 ding.volume = 0.2;
 
-//botClick is hint total
+$('.ansBut').attr('disabled', true);
 var errTotal = 1;
 var botInx = -1;
 var lastInpInx;
