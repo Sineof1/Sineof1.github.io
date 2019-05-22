36f830d6ecb3af186f5adbb1c4c988fde4794ff2
Sineof1 <Sineof1@users.noreply.github.com>
2019-05-21 20:48:20 -0400

    Update tutorbot.js

--- tutorbot/tutorbot.js
+++ tutorbot/tutorbot.js
@@ -531,7 +531,7 @@ function hintGroup1() {
                        .then(function(){botUI.message.bot({type : 'html', delay: 2000, loading: true, content: $('#hint1a').html()});})
                        .then(function(){$(".botui").animate({ scrollTop: $('.botui').prop("scrollHeight")}, 1000);})
                        .then(function(){
-                          hint1Count += 1; setTimeout(function(){botresponse.play();}, 2000);$('.bot').css('pointer-events', 'all');
+                          hint1Count += 1; setTimeout(function(){botresponse.play();$('.bot').css('pointer-events', 'all');}, 2000);
                        })
                        .then(function(){var d = new Date(); $('#botImage').attr('src', 'blinkbot.gif?' + d.getTime());});
                   }
