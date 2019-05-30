6b2b5d096f131792b932c02f8db2ceca3c31758c
Sineof1 <Sineof1@users.noreply.github.com>
2019-05-30 18:34:01 -0400

    Update tutorbot.js

--- tutorbot/tutorbot.js
+++ tutorbot/tutorbot.js
@@ -633,8 +633,8 @@ function hintGroup2() {
                   if (hint4Count >= 1) var hint4Class = 'botButVisited';
                   else var hint4Class = 'botBut';
                   return botUI.action.button({action: [
-                                               {cssClass : hint3Class, text : 'Can you teach me about this?', value : 'bothint3'},
-                                               {cssClass : hint4Class, text : 'What do I do here?', value : 'bothint4'},
+                                               {cssClass : hint3Class, text : 'I want to learn more about this.', value : 'bothint3'},
+                                               {cssClass : hint4Class, text : 'I want to learn how to do this.', value : 'bothint4'},
                                                {cssClass : 'exampBut', text : 'Show me an example.', value : 'bothintExamp'},
                                                {cssClass : 'exampBut', text : 'Show me a video.', value : 'bothintVid'},
                                                {cssClass : ansButReady, text : 'Just give me the answer.', value : 'bothintA2'}
@@ -710,8 +710,8 @@ function hintGroup3() {
                   if (hint6Count >= 1) var hint6Class = 'botButVisited';
                   else var hint6Class = 'botBut';
                   return botUI.action.button({action: [
-                     {cssClass : hint5Class, text : 'Can you teach me about this?', value : 'bothint5'},
-                     {cssClass : hint6Class, text : 'What do I do here?', value : 'bothint6'},
+                     {cssClass : hint5Class, text : 'I want to learn more about this.', value : 'bothint5'},
+                     {cssClass : hint6Class, text : 'I want to learn how to do this.', value : 'bothint6'},
                      {cssClass : 'exampBut', text : 'Show me an example.', value : 'bothintExamp'},
                      {cssClass : 'exampBut', text : 'Show me a video.', value : 'bothintVid'},
                      {cssClass : ansButReady, text : 'Just give me the answer.', value : 'bothintA3'}
