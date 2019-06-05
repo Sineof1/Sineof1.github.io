18bed14591d1155da097de4069ba58e87ff2ca40
Sineof1 <Sineof1@users.noreply.github.com>
2019-06-05 15:35:14 -0400

    Update tutorbot.js

--- tutorbot/tutorbot.js
+++ tutorbot/tutorbot.js
@@ -374,8 +374,7 @@ $('#jit2').offset({'left' : $('#demo_grapher').offset().left - 125, 'top' : $('#
 $('#jit3').offset({'left' : $('#equation3').offset().left, 'top' : $('#equation3').offset().top + 85});
 $('#jit4').offset({'left' : $('#equation3').offset().left, 'top' : $('#equation3').offset().top + 85});
 
-$(document).on('click dblclick', '.bot', function(evt) {
-   if (evt.type === 'dblclick') botClick -= 2;
+$(document).on('click', '.bot', function(evt) {
    botClick += 1;
    botInx = -1;
    botUI.message.removeAll();
