3f44faa9e7819994298e73518cbc59017cac6ece
Sineof1 <Sineof1@users.noreply.github.com>
2019-06-09 20:11:54 -0400

    Update tutorbot.js

--- tutorbot/tutorbot.js
+++ tutorbot/tutorbot.js
@@ -588,7 +588,7 @@ function startVid2() {
 function hintGroup0() {
   botInx += 1;
   setTimeout(function(){$('.botui').prop('scrollTop', 0);}, 500);
-  botUI.message.bot({type : 'html', content: 'Click on a point 🔵 to draw a <strong>slope triangle</strong>. Don\'t hold down the mouse button. Just click and move. 😃'})
+  botUI.message.bot({type : 'html', content: 'Click on a point 🔵 to draw a <strong>slope triangle</strong>.<br /><br />Don\'t hold down the mouse button. Just click and move. 😃'})
                .then(function(){
                   return botUI.action.button({action: [
                      {cssClass : 'botBut', text : 'I want to learn more about this.', value : 'bothint0'},
@@ -646,7 +646,7 @@ function hintGroup0() {
 function hintGroup1() {
   botInx += 1;
   setTimeout(function(){$('.botui').prop('scrollTop', 0);}, 500);
-  botUI.message.bot({type : 'html', content: 'Okay, so you want to determine the <strong>slope</strong> of <strong>line g</strong>.  😃'})
+  botUI.message.bot({type : 'html', content: 'Thanks for talking with me!<br /><br />Okay, so you want to determine the <strong>slope</strong> of <strong>line g</strong>.  😃'})
                .then(function(){
                   if (hint1Count >= 1 && hint2Count >= 1) var ansButReady = 'ansButReady';
                   else var ansButReady = 'ansBut';
@@ -761,7 +761,7 @@ function hintGroup1() {
 function hintGroup2() {
   botInx += 1;
   setTimeout(function(){$('.botui').prop('scrollTop', 0);}, 500);
-  botUI.message.bot({type : 'html', content: 'Now you want to complete the <strong>equation</strong> y = ⬜x for <strong>line g</strong>.  😊'})
+  botUI.message.bot({type : 'html', content: 'Oh, good! Let\'s talk.<br /><br />Now you want to complete the <strong>equation</strong> y = ⬜x for <strong>line g</strong>.  😊'})
                .then(function(){
                   if (hint3Count >= 1 && hint4Count >= 1) var ansButReady = 'ansButReady';
                   else var ansButReady = 'ansBut';
