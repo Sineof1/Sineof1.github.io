5ac2f4e61edfac0d37ffa091267f703fc0688994
Sineof1 <Sineof1@users.noreply.github.com>
2019-06-22 20:39:29 -0400

    Add files via upload

---
+++ scala/precache-manifest.f4610df9b2a938e052055230d9b2426e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a2df56376aa6e5af636597b9a1f788",
+    "url": "/scala/index.html"
+  },
+  {
+    "revision": "636063af3bca0f4e7661",
+    "url": "/scala/static/css/main.69fa68c0.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/scala/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "636063af3bca0f4e7661",
+    "url": "/scala/static/js/main.12b8872d.chunk.js"
+  },
+  {
+    "revision": "7a8a85cace784ba25a95",
+    "url": "/scala/static/js/runtime~main.03457a72.js"
+  }
+]);
