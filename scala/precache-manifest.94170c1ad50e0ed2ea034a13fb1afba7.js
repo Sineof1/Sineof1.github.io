5624c78b2fbc855259b7cc20349a268cb147403f
Sineof1 <Sineof1@users.noreply.github.com>
2019-06-23 10:13:07 -0400

    Add files via upload

---
+++ scala/precache-manifest.94170c1ad50e0ed2ea034a13fb1afba7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efdbb813609e08d5b1cb82e95620f6c",
+    "url": "/scala/index.html"
+  },
+  {
+    "revision": "5f123f64d0ed5f9c7016",
+    "url": "/scala/static/css/main.997b89f0.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/scala/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "5f123f64d0ed5f9c7016",
+    "url": "/scala/static/js/main.d3d318a4.chunk.js"
+  },
+  {
+    "revision": "7a8a85cace784ba25a95",
+    "url": "/scala/static/js/runtime~main.03457a72.js"
+  }
+]);
--- scala/service-worker.js
+++ scala/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scala/precache-manifest.6f609a935ec4dc8c660e219c1454915b.js"
+  "/scala/precache-manifest.94170c1ad50e0ed2ea034a13fb1afba7.js"
 );
 
 self.addEventListener('message', (event) => {
