acc24936cd6d98e0e8c352c7cc0256c5f8c1194c
Sineof1 <Sineof1@users.noreply.github.com>
2019-09-20 10:34:54 -0400

    Add files via upload

---
+++ scalamath/comparing_two_fractions/precache-manifest.c5d6fb5f3c583d13a56b204c8bd1ac49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875ef3c81471687996a4e3cb94f908b9",
+    "url": "/scalamath/comparing_two_fractions/index.html"
+  },
+  {
+    "revision": "da64c27255d4a8067d6f",
+    "url": "/scalamath/comparing_two_fractions/static/css/2.2e34ae7f.chunk.css"
+  },
+  {
+    "revision": "39d89ca220e42af2dad9",
+    "url": "/scalamath/comparing_two_fractions/static/css/main.17c8b3cb.chunk.css"
+  },
+  {
+    "revision": "da64c27255d4a8067d6f",
+    "url": "/scalamath/comparing_two_fractions/static/js/2.dc9218fd.chunk.js"
+  },
+  {
+    "revision": "39d89ca220e42af2dad9",
+    "url": "/scalamath/comparing_two_fractions/static/js/main.60f5ea03.chunk.js"
+  },
+  {
+    "revision": "ebe5443be4c4933ef18d",
+    "url": "/scalamath/comparing_two_fractions/static/js/runtime~main.df36ffdb.js"
+  }
+]);
