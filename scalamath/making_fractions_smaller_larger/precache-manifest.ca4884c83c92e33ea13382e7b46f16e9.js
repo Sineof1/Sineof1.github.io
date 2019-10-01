ef04716ff588a131bc01081e756f5d759fe47772
Josh Fisher <jaydamion@gmail.com>
2019-09-30 20:16:59 -0400

    Adding scala.

---
+++ scalamath/making_fractions_smaller_larger/precache-manifest.ca4884c83c92e33ea13382e7b46f16e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03110e89eae6a63bdb8016a92cbbceef",
+    "url": "/scalamath/making_fractions_smaller_larger/index.html"
+  },
+  {
+    "revision": "f59ae793cec0d6c4df81",
+    "url": "/scalamath/making_fractions_smaller_larger/static/css/2.2e34ae7f.chunk.css"
+  },
+  {
+    "revision": "53e255a0d9f128318a7b",
+    "url": "/scalamath/making_fractions_smaller_larger/static/css/main.960bb231.chunk.css"
+  },
+  {
+    "revision": "f59ae793cec0d6c4df81",
+    "url": "/scalamath/making_fractions_smaller_larger/static/js/2.63a9a9a0.chunk.js"
+  },
+  {
+    "revision": "53e255a0d9f128318a7b",
+    "url": "/scalamath/making_fractions_smaller_larger/static/js/main.0c9b0ec7.chunk.js"
+  },
+  {
+    "revision": "dbfc88bb27f7f8d24ad6",
+    "url": "/scalamath/making_fractions_smaller_larger/static/js/runtime~main.855418ba.js"
+  }
+]);
