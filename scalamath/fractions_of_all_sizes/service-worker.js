e496d05154e9212a4ca30d6fe62bb6c62f802a9a
Josh Fisher <jaydamion@gmail.com>
2019-10-10 21:39:18 -0400

    Added padding and highlighting.

--- scalamath/fractions_of_all_sizes/service-worker.js
+++ scalamath/fractions_of_all_sizes/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scalamath/fractions_of_all_sizes/precache-manifest.64ce0ea199ad43842ba7c23ee7a5df59.js"
+  "/scala/fractions_of_all_sizes/precache-manifest.10ee22b8f81a31d07efb798b6380ff4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scalamath/fractions_of_all_sizes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scala/fractions_of_all_sizes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
