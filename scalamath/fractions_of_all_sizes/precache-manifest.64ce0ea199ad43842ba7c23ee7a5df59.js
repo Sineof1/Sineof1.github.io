f82bde9f781a83e6ab67f92e6b1617db334814c0
Josh Fisher <jaydamion@gmail.com>
2019-10-10 21:02:46 -0400

    Added scala to demo video Learn.

---
+++ scalamath/fractions_of_all_sizes/precache-manifest.64ce0ea199ad43842ba7c23ee7a5df59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631441b74a96305c2eb4a82b13c2a825",
+    "url": "/scalamath/fractions_of_all_sizes/index.html"
+  },
+  {
+    "revision": "ecb83c2ae77104b3f4f9",
+    "url": "/scalamath/fractions_of_all_sizes/static/css/2.2e34ae7f.chunk.css"
+  },
+  {
+    "revision": "cc92d6d3c55ee25982e4",
+    "url": "/scalamath/fractions_of_all_sizes/static/css/main.1e5820e2.chunk.css"
+  },
+  {
+    "revision": "ecb83c2ae77104b3f4f9",
+    "url": "/scalamath/fractions_of_all_sizes/static/js/2.8a09c756.chunk.js"
+  },
+  {
+    "revision": "cc92d6d3c55ee25982e4",
+    "url": "/scalamath/fractions_of_all_sizes/static/js/main.5a964523.chunk.js"
+  },
+  {
+    "revision": "aa03b119727235404170",
+    "url": "/scalamath/fractions_of_all_sizes/static/js/runtime~main.058f5286.js"
+  }
+]);
