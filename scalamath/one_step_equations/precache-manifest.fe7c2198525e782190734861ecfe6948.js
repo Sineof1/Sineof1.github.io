4fc3d1e44c3f2f39a16ded10f112135a62587901
Josh Fisher <jaydamion@gmail.com>
2019-10-19 21:12:39 -0400

    New Scala for demo of Getting Ready page in txtbook redesign.

---
+++ scalamath/one_step_equations/precache-manifest.fe7c2198525e782190734861ecfe6948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c74d1de8aa319144acc11376f0d7d41",
+    "url": "/scalamath/one_step_equations/index.html"
+  },
+  {
+    "revision": "ecb83c2ae77104b3f4f9",
+    "url": "/scalamath/one_step_equations/static/css/2.2e34ae7f.chunk.css"
+  },
+  {
+    "revision": "8e4fa334dc95c379c163",
+    "url": "/scalamath/one_step_equations/static/css/main.95b6065a.chunk.css"
+  },
+  {
+    "revision": "ecb83c2ae77104b3f4f9",
+    "url": "/scalamath/one_step_equations/static/js/2.8a09c756.chunk.js"
+  },
+  {
+    "revision": "8e4fa334dc95c379c163",
+    "url": "/scalamath/one_step_equations/static/js/main.ab7b9c88.chunk.js"
+  },
+  {
+    "revision": "ae7b0b07f851f15bfd5d",
+    "url": "/scalamath/one_step_equations/static/js/runtime~main.61e87a93.js"
+  }
+]);
