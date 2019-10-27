c74dff92b7311131eed5719d49973f61e07ccb70
Josh Fisher <jfisher@carnegielearning.com>
2019-10-27 17:01:11 -0400

    Going multiple-choice.

---
+++ scalamath/add_subtract_any_fractions/precache-manifest.36794c20fa32ff59772d73496b162392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd346137608a814dd4c6c9b384e438e",
+    "url": "/scalamath/add_subtract_any_fractions/index.html"
+  },
+  {
+    "revision": "db95909ac707b5adf708",
+    "url": "/scalamath/add_subtract_any_fractions/static/css/main.40a4a5b0.chunk.css"
+  },
+  {
+    "revision": "b8e447deabcb50a66688",
+    "url": "/scalamath/add_subtract_any_fractions/static/js/2.fc7f80dd.chunk.js"
+  },
+  {
+    "revision": "db95909ac707b5adf708",
+    "url": "/scalamath/add_subtract_any_fractions/static/js/main.f5ea8826.chunk.js"
+  },
+  {
+    "revision": "314ba9de284991b877e8",
+    "url": "/scalamath/add_subtract_any_fractions/static/js/runtime~main.ea56a46c.js"
+  }
+]);
