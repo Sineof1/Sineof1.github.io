4f97325fdf2463caa9e3cdc04d699d7d29989d01
Sineof1 <Sineof1@users.noreply.github.com>
2019-12-04 13:18:30 -0500

    Add files via upload

---
+++ scalamath/multiplying_fractions/precache-manifest.c6e0fcf148f1dd8c8094db6457fec2e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcee3b7731aff3c798e0a94b560b684",
+    "url": "/scalamath/multiplying_fractions/index.html"
+  },
+  {
+    "revision": "b5326d412eeaff007bde",
+    "url": "/scalamath/multiplying_fractions/static/css/main.8079ed55.chunk.css"
+  },
+  {
+    "revision": "5b4ccd1be9dfdbcfbe65",
+    "url": "/scalamath/multiplying_fractions/static/js/2.c94bf451.chunk.js"
+  },
+  {
+    "revision": "b5326d412eeaff007bde",
+    "url": "/scalamath/multiplying_fractions/static/js/main.82fc3fe3.chunk.js"
+  },
+  {
+    "revision": "f25c72bced9cecf52cba",
+    "url": "/scalamath/multiplying_fractions/static/js/runtime~main.92cdb4a5.js"
+  }
+]);
