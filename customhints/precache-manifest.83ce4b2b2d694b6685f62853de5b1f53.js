ea97c92337c0f7ae411eb885acdbb4d8531aca18
Sineof1 <Sineof1@users.noreply.github.com>
2020-06-19 22:25:39 -0400

    Add files via upload

---
+++ customhints/precache-manifest.83ce4b2b2d694b6685f62853de5b1f53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56236c4aab0ecca880c4faa5e7788ce8",
+    "url": "/customhints/index.html"
+  },
+  {
+    "revision": "6dd596264a22617cd54e",
+    "url": "/customhints/static/css/main.76496f34.chunk.css"
+  },
+  {
+    "revision": "d18c5590f1bdae421103",
+    "url": "/customhints/static/js/2.fa1485b6.chunk.js"
+  },
+  {
+    "revision": "1e28c59aef30c4685e8643ac1e18c912",
+    "url": "/customhints/static/js/2.fa1485b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd596264a22617cd54e",
+    "url": "/customhints/static/js/main.f271d984.chunk.js"
+  },
+  {
+    "revision": "cdb3427ae6ee4a49ee04",
+    "url": "/customhints/static/js/runtime-main.c165628e.js"
+  },
+  {
+    "revision": "9810d917955c6a64ec0fd409169c89b7",
+    "url": "/customhints/static/media/hintbot.9810d917.png"
+  }
+]);
